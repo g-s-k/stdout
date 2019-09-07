c63675782eb03b0add484a323014f5f8ee702bfb
George Kaplan <george@georgekaplan.xyz>
2019-09-07 07:46:22 -0400

    use macro in checked rust version

--- main_safe.rs
+++ main_safe.rs
@@ -3,15 +3,19 @@ use std::{
     process,
 };
 
-const HELLO: &[u8] = b"hello world\n";
-
-fn main() {
-    for _ in 0..200 {
+macro_rules! print_safe {
+    ($fmt:expr $(, $e:expr)*) => {{
         let mut std_out = io::stdout();
-        match std_out.write_all(HELLO) {
+        match std_out.write_fmt(format_args!($fmt $(, $e:expr)*)) {
             Err(ref e) if e.kind() == ErrorKind::BrokenPipe => process::exit(0),
             Err(e) => panic!("{:?}", e),
             Ok(_) => (),
         }
+    }}
+}
+
+fn main() {
+    for _ in 0..200 {
+        print_safe!("hello world\n");
     }
 }
